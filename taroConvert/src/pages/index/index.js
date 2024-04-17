838066e356ad7fd7226b79d73a73e7d5845ac1ca
yongzheng <yongzheng@trip.com>
2024-04-17 17:52:06 +0800

    import tdesign

--- taroConvert/src/pages/index/index.js
+++ taroConvert/src/pages/index/index.js
@@ -20,7 +20,7 @@ const defaultAvatarUrl =
   'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
 cacheOptions.setOptionsToCache({
   data: {
-    motto: 'Hello World',
+    motto: 'Hello World2',
     userInfo: {
       avatarUrl: defaultAvatarUrl,
       nickName: '',
